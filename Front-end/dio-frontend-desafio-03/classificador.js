f05c76a341abbb68583ba3f84885d0c99701120c
Catarina Coelho <k.tarina56@gmail.com>
2024-10-24 14:09:00 -0300

    limpeza de codigo

--- Front-end/dio-frontend-desafio-03/classificador.js
+++ Front-end/dio-frontend-desafio-03/classificador.js
@@ -1,5 +1,5 @@
-const xp= 8999;
-const nome = "Lucas";
+const xp= 9999;
+const nome = "Herói";
 
 function classificaNivel (xp, nome){
     let nivelHeroi;
@@ -34,21 +34,3 @@ function classificaNivel (xp, nome){
 };
 
 classificaNivel(xp, nome);
-
-//Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-
-//Se XP for menor do que 1.000 = Ferro
-//Se XP for entre 1.001 e 2.000 = Bronze
-//Se XP for entre 2.001 e 5.000 = Prata
-//Se XP for entre 5.001 e 7.000 = Ouro
-//Se XP for entre 7.001 e 8.000 = Platina
-//Se XP for entre 8.001 e 9.000 = Ascendente
-//Se XP for entre 9.001 e 10.000= Imortal
-//Se XP for maior ou igual a 10.001 = Radiante
-
-//## Saída
-
-//Ao final deve se exibir uma mensagem:
-//"O Herói de nome **{nome}** está no nível de **{nivel}**"
-
- 
